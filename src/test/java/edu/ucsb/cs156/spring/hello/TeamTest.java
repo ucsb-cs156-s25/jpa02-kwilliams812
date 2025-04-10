df67edf1a3d04c22ba940d524cc27a4a118eebfe
Josh1182021 <66659887+Josh1182021@users.noreply.github.com>
2025-04-09 21:31:43 -0700

    KW - 100percent coverage on Team

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -1,6 +1,8 @@
 package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,8 +21,58 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
    
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void equals_on_same_object() {
+        assert(team.equals(team));
+    }
 
+    @Test
+    public void equals_on_diff_class() {
+        assertFalse(team.equals("test-team"));
+    }
+
+    @Test
+    public void equals_on_equal_obj() {
+        team.addMember("test-member");
+        Team other_team = new Team("test-team");
+        other_team.addMember("test-member");
+        assert(team.equals(other_team));
+    }
+
+    @Test
+    public void equals_on_diff_names() {
+        team.addMember("test-member");
+        Team other_team = new Team("test-team-different");
+        other_team.addMember("test-member");
+        assertFalse(team.equals(other_team));
+    }
+
+    @Test
+    public void equals_on_diff_members() {
+        team.addMember("test-member");
+        Team other_team = new Team("test-team");
+        other_team.addMember("test-member-different");
+        assertFalse(team.equals(other_team));
+    }
+
+    @Test
+    public void equals_on_not_equal_obj() {
+        team.addMember("test-member");
+        Team other_team = new Team("test-team-different");
+        other_team.addMember("test-member-different");
+        assertFalse(team.equals(other_team));
+    }
+
+    @Test
+    public void hashCode_gives_expected_value() {
+        // We hard code this to an expected value to get around equivalent mutations
+        int result = team.hashCode();
+        int expectedResult = -1226298695;
+        assertEquals(expectedResult, result);
+    }
 }
